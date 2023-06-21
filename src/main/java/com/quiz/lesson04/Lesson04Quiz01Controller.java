ccc740c164818b6996b4c71b709cabb30bd6c459
test <aqumon12@naver.com>
2023-06-21 14:05:47 +0900

    home 230621

--- src/main/java/com/quiz/lesson04/Lesson04Quiz01Controller.java
+++ src/main/java/com/quiz/lesson04/Lesson04Quiz01Controller.java
@@ -1,10 +1,45 @@
 package com.quiz.lesson04;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.quiz.lesson04.bo.SellerBO;
+import com.quiz.lesson04.domain.Seller;
 
 @RequestMapping("/lesson04/quiz01")
 @Controller
 public class Lesson04Quiz01Controller {
-
+	
+	@Autowired
+	private SellerBO sellerBO;
+	
+	@GetMapping("/add_seller_view")
+	public String addSellerView() {
+		return "lesson04/addSeller";
+	}
+	
+	@PostMapping("/add_seller")
+	public String addSeller(
+			@RequestParam("nickname") String nickname,
+			@RequestParam(value = "profileImageUrl", required = false) String profileImageUrl,
+			@RequestParam(value = "temperature", required = false) double temperature) {
+		
+		sellerBO.addSeller(nickname, profileImageUrl, temperature);
+		
+		
+		return "lesson04/afterAddSeller";
+	}
+	
+	@GetMapping("/seller_info")
+	public String sellerInfo(Model model) {
+		Seller seller = sellerBO.getSellerInfo();
+		model.addAttribute("result", seller);
+		
+		return "lesson04/sellerInfo";
+	}
 }
--- src/main/java/com/quiz/lesson04/dao/SellerMapper.java
+++ src/main/java/com/quiz/lesson04/dao/SellerMapper.java
@@ -1,5 +1,18 @@
 package com.quiz.lesson04.dao;
 
-public interface SellerMapper {
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
+import com.quiz.lesson04.domain.Seller;
+
+@Repository
+public interface SellerMapper {
+	
+	public void insertSeller(
+			@Param("nickname") String nickname,
+			@Param("profileImageUrl") String profileImageUrl,
+			@Param("temperature") double temperature);
+	
+	public Seller sellectSellerInfo();
 }
+	
